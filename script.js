eb7fe0f71673b2a62cbf2b0d8a4ac7108461cffa
Tusharmgs <tusharkesharimgs88@gmail.com>
2025-05-09 23:56:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+  const navRight = document.querySelector(".nav-right");
+
+  toggleBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    navRight.classList.toggle("active");
+  });
+});
